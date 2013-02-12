dd4068c3b45ec17ce158839c9e8fe52ec87668ea
glindste <gustaflindstedt@gmail.com>
2013-02-12 15:41:54 +0100

    initial conceptnet interface

--- kexbot.py
+++ kexbot.py
@@ -3,6 +3,7 @@
 import nltk
 import cmd
 
+from cnet_client import *
 
 class kexbot(cmd.Cmd):
 
@@ -10,6 +11,9 @@ class kexbot(cmd.Cmd):
 
     def do_EOF(self, line):
         return True
+
+    def do_lookup(self, line):
+        print lookup(*line.split(' '))
 
     def default(self, line):
         tokens = nltk.word_tokenize(line)
