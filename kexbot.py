9094485ce4f09b50ef1b59e7df1c74105f9c61da
glindste <gustaflindstedt@gmail.com>
2013-02-15 14:16:02 +0100

    kexbot logo

--- kexbot.py
+++ kexbot.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python2
 
+import sys
 import nltk
 import cmd
 
 
+from termcolor import cprint 
+from colorama import init
+init(strip=not sys.stdout.isatty()) # strip colors if stdout is redirected
 import interpreter as it
 import cnet_client as cnet
+from pyfiglet import figlet_format 
 
 class Kexbot(cmd.Cmd):
 
+    print "\n"
+    cprint(figlet_format('kexbot!', font='starwars'),
+	       'red', attrs=['bold'])
+    print "\n"
     prompt = ">"
 
     def do_EOF(self, line):
